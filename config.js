61ba6b9366f20ac4776f70868f0bfecd721362c7
Hansika Lakshan <127050583+HansikaLakshanIhalage@users.noreply.github.com>
2023-12-21 17:33:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94783379511', 'me', true],
+  ['94783379511', 'sika', true], 
   [''] 
 ] //Number of owners
 
